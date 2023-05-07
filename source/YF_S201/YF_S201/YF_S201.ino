e7c0014731f91dfee7dacebaa14927add32e1e52
DESKTOP-ACR98V8\stand <jplee04@naver.com>
2023-05-07 11:46:33 +0900

    05.07 minor changes, Litre per hour to min

--- source/YF_S201/YF_S201/YF_S201.ino
+++ source/YF_S201/YF_S201/YF_S201.ino
@@ -1,34 +1,32 @@
 volatile int flow_frequency;
-unsigned int l_hour;      // Litres per hour
+float l_minute;      // Litres per minute
 unsigned char FLOW_PIN = 2;
 unsigned long current_time;
 unsigned long cloop_time;
 
 void flow(){
-  Serial.println("DETECTED");
   flow_frequency++;
 }
 void setup() {
   pinMode(FLOW_PIN, INPUT);
-  digitalWrite(FLOW_PIN, HIGH);
   Serial.begin(9600);
   attachInterrupt(0, flow, RISING);
   sei();
-  current_time = millis();
-  cloop_time = current_time;
+  cloop_time = millis();
 
 }
 
 void loop() {
   current_time = millis();
   // Every second, calculate and print litres/hour
-  if(current_time >= (cloop_time + 1000))  {
-    detachInterrupt(0);
+  if(current_time == (cloop_time + 1000))  {
     cloop_time = current_time; // Updates cloopTime
+    Serial.println(flow_frequency);
     // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
-    l_hour = (flow_frequency * 60 / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
+    // 60Hz 신호가 계속 잡혀서 -60하여 보상함
+    l_minute = ((flow_frequency - 60) / 7.5); // (Pulse frequency x 1 min) / 7.5Q = flowrate in L/hour
     flow_frequency = 0; // Reset Counter
-    Serial.print(l_hour, DEC); // Print litres/hour
-    Serial.println(" L/hour");
+    Serial.print(l_minute, 3); // Print litres/min
+    Serial.println(" L/min");
   }
 }
