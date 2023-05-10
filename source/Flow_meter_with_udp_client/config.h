3442f3fb469f440089c3cf386363502fd63570f2
DESKTOP-ACR98V8\stand <jplee04@naver.com>
2023-05-10 18:36:00 +0900

    fix. untracked files

--- source/Flow_meter_with_udp_client/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef MyLibrary_H
-#define MyLibrary_H
-const char* ssid = "carboncheck";
-const char* password = "18991899\"";
-const char* CWJAP_prefix = "AT+CWJAP=\"carboncheck\",\"";
-const char* CIPSTART_UDP = "AT+CIPSTART=\"UDP\",\"10.42.0.1\",18999";
-const char* CIPSEND_UDP= "AT+CIPSEND=";
-#endif
