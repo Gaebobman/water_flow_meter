338f740e766ac5534abe9857e21d23b3915e40f9
DESKTOP-ACR98V8\stand <jplee04@naver.com>
2023-05-10 18:26:00 +0900

    05.10 feat.Change the protocol from tcp to udp

--- source/Flow_meter_with_udp_client/Flow_meter_with_udp_client.ino
+++ source/Flow_meter_with_udp_client/Flow_meter_with_udp_client.ino
@@ -1,17 +1,16 @@
 #include <SoftwareSerial.h>
 #include "config.h"
-// Define the SSID and password of the hotspot,
-// IP address and port number of the Linux server
+// Define AT commands for ESP01 
 
 #define ESP_RX 1
-#define ESP_TX 0  // Define the RX and TX pins of ESP01 module
+#define ESP_TX 0 // Define the RX and TX pins of ESP01 module
 #define YF_PIN 2  // Define the interrupt pin of YF-S201 module
 
 SoftwareSerial esp(ESP_RX, ESP_TX);  // Create a SoftwareSerial object
 volatile int pulse_count = 0;        // Define a variable to store the pulse count
 float flow_rate = 0;                 // Define a variable to store the flow rate
 unsigned long last_time = 0;         // Define a variable to store the last time of calculation
-unsigned long interval = 1000;       // Define a variable to store the interval of calculation (in milliseconds)
+unsigned long interval = 2000;       // Define a variable to store the interval of calculation (in milliseconds)
 char buffer[100];                    // Define a buffer to store the AT commands and responses
 unsigned long start;
 void setup() {
@@ -20,12 +19,10 @@ void setup() {
   // Initialize the interrupt pin of YF-S201 module
   pinMode(YF_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(YF_PIN), pulse, RISING);
-  esp.print("AT+RST");
-  delay(5000);
+  delay(3000);
   // Connect to the hotspot using AT commands
-  // sendCommand(CWJAP_prefix , password , "\r\n", 'K', 15000);
-  // Connect to the Linux server using AT commands
-  sendCommand(CIPSTART_prefix, server_port, "\r\n", 'K', 5000);
+  sendCommand(CIPSTART_UDP, "", "\r\n", 'K', 2000);
+  delay(3000);
   // RETURNS "CONNECT\nOK\r\n"
 }
 
@@ -36,13 +33,9 @@ void loop() {
     last_time = millis();
     flow_rate = pulse_count / 7.5;  // flow rate (L/min) = pulse frequency (Hz) / 7.5
     pulse_count = 0;                // Reset the pulse count
-    // Print the flow rate to the serial monitor
-    Serial.print("Flow rate: ");
-    Serial.print(flow_rate);
-    Serial.println(" L/min");
     // Send the flow rate to the Linux server using AT commands
-    sendCommand("AT+CIPSEND=", String(flow_rate).c_str(), "\r\n", 'K', 1000);
-    // sendCommand("", "", "\r\n", 'K', 1000);
+    // sendCommand("AT+CIPSEND=", "", "\r\n", '>', 2000); // TCP SEND
+    sendCommand(CIPSEND_UDP, String(flow_rate).c_str(), "\r\n", 'K', 2000);  // UDP SEND
   }
 }
 
@@ -59,29 +52,33 @@ void sendCommand(const char* prefix, const char* data, const char* suffix, char 
   if (strstr(prefix, "AT+CIPSEND")) {
     // Code block for CIPSEND command
     sprintf(buffer, "%s%d%s", prefix, strlen(data), suffix);  // Use sprintf to format the string with the data length
-    esp.print(buffer);
+    Serial.print(buffer);
+    // esp.print(buffer);   // 왜 이렇게 되는지 모르겠음 
     // Wait for '>' Before sending Water Usage data
     start = millis();
-    while (esp.readBytesUntil('>', buffer, sizeof(buffer)) != 0) {
+    while (esp.readBytesUntil(expected, buffer, sizeof(buffer)) != 0) {
       if (millis() - start >= timeout) {
-        Serial.print("Timeout1");
-        return;
+        Serial.println("Timeout1");
+        break;
       }
     }
+    Serial.println(data);
+
     // If '>' is detected
-    memset(buffer, 0, sizeof(char) * strlen(data));
+    memset(buffer, 0, sizeof(buffer));
     strcat(buffer, data);
     strcat(buffer, suffix);
-    esp.print(buffer);  // TCP SEND
+    esp.print(buffer);  // UDP SEND
+
     // Wait for response
     start = millis();
-    int rlen = esp.readBytes(buffer, )
     while (esp.readBytesUntil(expected, buffer, sizeof(buffer)) != 0) {
       if (millis() - start >= timeout) {
         Serial.println("Timeout2");
-        return;
+        break;
       }
     }
+
   } else {
     // Code block for the other commands
     strcat(buffer, prefix);
